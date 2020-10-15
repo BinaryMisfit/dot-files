508f53e160bcc011240be4c9aa9f76251951ddb3
Willie Roberts <diagoza@me.com>
2020-10-15 10:36:19 +0200

    Added ability to install .dotfiles

--- default/scripts/.update_env.sh
+++ default/scripts/.update_env.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
 echo "Update environment"
+INSTALL_DOT_FILES=false
 UPDATE_DOT_FILES=true
 DOT_FILES=~/.dotfiles
 GIT=$(which git)
 OS_PREFIX=
-if [[ "${OSTYPE}" == "darwin"* ]]; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
   OS_PREFIX="osx"
-elif [[ "${OSTYPE}" == "linux-gnu" ]]; then
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
   OS_PREFIX=$(cat /etc/os-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print tolower($1)}')
 fi
 
-if [[ -z ${OS_PREFIX} ]]; then
-  echo "Unknown OS: ${OSTYPE}"
+if [[ -z $OS_PREFIX ]]; then
+  echo "Unknown OS: $OSTYPE"
   exit 1
 fi
 
-echo ${OS_PREFIX}
 if [ ! -d "$DOT_FILES" ]; then
   UPDATE_DOT_FILES=false
+  INSTALL_DOT_FILES=true
   echo "Shell configuration scripts not installed"
 fi
 
 if [ -z "$GIT" ]; then
   UPDATE_DOT_FILES=false
-  echo "Skipping dotfiles update (Git not found)"
+  INSTALL_DOT_FILES=false
+  echo "Skipping dotfiles (Git not found)"
+fi
+
+if [ "$INSTALL_DOT_FILES" == true ]; then
+  git clone https://github.com/BinaryMisfit/dot-files.git ~/.dotfiles --recurse-submodules
 fi
 
 if [ "$UPDATE_DOT_FILES" == true ]; then
-  pushd $DOT_FILES > /dev/null
-  git pull --recurse-submodules
-  popd > /dev/null
+  pushd $DOT_FILES &>/dev/null
+  git remote update --prune &>/dev/null
+  popd &>/dev/null
+fi
+
+if [ "$OS_PREFIX" == "osx" ]; then
+  echo "Checking brew"
+fi
+
+if [ "$OS_PREFIX" == "ubuntu" ]; then
+  echo "Checking apt"
 fi
 
 echo "Update completed"
