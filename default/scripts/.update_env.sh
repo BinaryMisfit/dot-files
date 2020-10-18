6a7b4c33c339b807a8c7aed8850ec74da462965a
Willie Roberts <diagoza@me.com>
2020-10-18 16:17:59 +0200

    Updated apt steps for ubuntu

--- default/scripts/.update_env.sh
+++ default/scripts/.update_env.sh
@@ -6,6 +6,10 @@ fi
 
 touch ~/.update_in_progress
 
+APT_ADD_SRC=$(which add-apt-repository)
+APT_APPS=~/.apt_apps
+APT_GET=$(which apt-get)
+APT_SOURCES=~/.apt_sources
 BREW=$(which brew)
 CONFIGURE_DOT_FILES=false
 DOT_FILES=~/.dotfiles
@@ -15,6 +19,9 @@ ENVIRONMENT=~/.environment.zsh
 GIT=$(which git)
 GREEN="\033[0;32m"
 INSTALL_DOT_FILES=false
+IS_SUDO=false
+MD5=
+MD5_APT_ADD_SRC=
 NC="\033[0m"
 NPM=$(which npm)
 OS_PREFIX=
@@ -22,28 +29,30 @@ PIP3=$(which pip3)
 RED="\033[0;31m"
 REPLACE="\e[1A\e[K"
 REPLACE2="\e[2A\e[K"
+SUDO=$(which sudo)
 UPDATE_DOT_FILES=true
 YELLOW="\033[0;33m"
+ZSH=
 
 STAGE=":: Verifying environment"
 printf "${NC}%s${NC}\n" "$STAGE"
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  OS_PREFIX="osx"
-elif [ "$OSTYPE" == "linux-gnu" ]; then
-  OS_PREFIX=$(cat /etc/os-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print tolower($1)}')
+  OS_PREFIX="OSX"
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+  OS_PREFIX=$(cat /etc/os-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print toupper($1)}')
 fi
 
-if [ -z $OS_PREFIX ]; then
+if [[ -z $OS_PREFIX ]]; then
   printf "${REPLACE}${NC}${STAGE}\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "Unknown OS $OSTYPE"
   exit 255
 fi
 
-if [ ! -d "$DOT_FILES" ]; then
+if [[ ! -d "$DOT_FILES" ]]; then
   UPDATE_DOT_FILES=false
   INSTALL_DOT_FILES=true
 fi
 
-if [ ! -f "$ENVIRONMENT" ]; then
+if [[ ! -f "$ENVIRONMENT" ]]; then
   touch "$ENVIRONMENT"
 fi
 
@@ -53,20 +62,37 @@ unset ITERM2_SQUELCH_MARK
 unset KEYTIMEOUT
 unset MD5_BREW_APPS
 source "$ENVIRONMENT"
-if [ -z $COLORTERM ]; then
+if [[ -z $COLORTERM ]]; then
   echo "export COLORTERM=truecolor" >> $ENVIRONMENT
 fi
-if [ -z $DEFAULT_USER ]; then
+
+if [[ -z $DEFAULT_USER ]]; then
   echo "export DEFAULT_USER=$(whoami)" >> $ENVIRONMENT
 fi
-if [ -z $ITERM2_SQUELCH_MARK ]; then
+
+if [[ -z $ITERM2_SQUELCH_MARK ]]; then
   echo "export ITERM2_SQUELCH_MARK=1" >> $ENVIRONMENT
 fi
-if [ -z $KEYTIMEOUT ]; then
+
+if [[ -z $KEYTIMEOUT ]]; then
   echo "export KEYTIMEOUT=1" >> $ENVIRONMENT
 fi
-printf "${REPLACE}${NC}${STAGE}\t${GREEN}%s${NC}\n" "${OS_PREFIX}"
 
+if [[ -f "$SUDO" ]]; then
+  if [[ "$OS_PREFIX" == "OSX" ]]; then
+    IS_SUDO=$(groups $USER | grep -w admin)
+  fi
+
+  if [[ "$OS_PREFIX" == "UBUNTU" ]]; then
+    IS_SUDO=$(groups $USER | grep -w sudo)
+  fi
+
+  if [[ "$IS_SUDO" != "" ]]; then
+    IS_SUDO=true
+  fi
+fi
+
+printf "${REPLACE}${NC}${STAGE}\t${GREEN}%s${NC}\n" "${OS_PREFIX}"
 STAGE=":: Verifying config files"
 printf "${NC}%s${NC}\n" "$STAGE"
 if [ -z "$GIT" ]; then
@@ -82,6 +108,7 @@ if [ "$INSTALL_DOT_FILES" == true ]; then
     printf "${REPLACE}${NC}${STAGE}\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "git clone failed"
     exit 255
   fi
+
   CONFIGURE_DOT_FILES=true
 fi
 
@@ -93,11 +120,13 @@ if [ "$UPDATE_DOT_FILES" == true ]; then
     printf "${REPLACE2}${NC}${STAGE}\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "git log failed"
     exit 255
   fi
+
   REMOTE_HEAD=$(eval $GIT ls-remote origin -h refs/heads/latest | cut -f1)
   if [ $? != 0 ]; then
     printf "${REPLACE2}${NC}${STAGE}\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "git ls-remote failed"
     exit 255
   fi
+
   if [ "$CURRENT_HEAD" != "$REMOTE_HEAD" ]; then
     printf "${REPLACE}${NC}${STAGE}\t${YELLOW}%s${NC}\t%s${NC}\n" "UPDATE"
     CONFIGURE_DOT_FILES=true
@@ -107,6 +136,7 @@ if [ "$UPDATE_DOT_FILES" == true ]; then
       exit 255
     fi
   fi
+
   DOT_FILES_PUSH=$(eval $GIT status -s | wc -l)
   if [ $? != 0 ]; then
     printf "${REPLACE2}${NC}${STAGE}\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "git status failed"
@@ -115,6 +145,7 @@ if [ "$UPDATE_DOT_FILES" == true ]; then
   if [ "$DOT_FILES_PUSH" -gt "0" ]; then
     CONFIGURE_DOT_FILES=true
   fi
+
   popd &>/dev/null
 fi
 
@@ -124,6 +155,7 @@ if [ "$CONFIGURE_DOT_FILES" == true ]; then
     printf "${REPLACE}${NC}${STAGE}\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "install script missing"
     exit 255
   fi
+
   eval $DOT_FILES_INSTALL &>/dev/null
   if [ $? != 0 ]; then
     printf "${REPLACE}${NC}${STAGE}\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "install failed"
@@ -143,10 +175,12 @@ else
   printf "${REPLACE}${NC}${STAGE}\t${GREEN}%s${NC}\n" "OK"
 fi
 
-if [ "$OS_PREFIX" == "osx" ]; then
-  echo ":: Verifying ``brew``"
-  if [ -z $BREW ]; then
-    echo " :: Installing ``brew``"
+STAGE=":: Verifying packages"
+printf "${NC}%s${NC}\n" "$STAGE"
+if [ "$OS_PREFIX" == "OSX" ]; then
+  printf "${REPLACE}${NC}${STAGE}\t${YELLOW}%s${NC}\t%s${NC}\n" "CHECKING"
+  if [ ! -z $BREW ]; then
+    printf "${REPLACE}${NC}${STAGE}\t\t${YELLOW}%s${NC}\t%s${NC}\n" "INSTALLING" "brew"
     CI=1 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &>/dev/null
     if [ $? != 0 ]; then
       echo " :: ERROR: ``brew`` installation failed"
@@ -196,70 +230,128 @@ if [ "$OS_PREFIX" == "osx" ]; then
   fi
 fi
 
-if [ "$OS_PREFIX" == "ubuntu" ]; then
-  echo ":: Verifying ``apt-get``"
-  APT_UPDATE=$(sudo apt-get -qq upgrade --dry-run)
-  if [ $? != 0 ]; then
-    echo " :: ERROR: ``apt-get`` upgrade failed"
-    exit 255
-  fi
-  if [ ! -z "$APT_UPDATE" ]; then
-    echo " :: Updating ``apt-get`` packages"
-    APT_UPGRADE=$(sudo apt-get -qq upgrade -y)
-    if [ $? != 0 ]; then
-      echo " :: ERROR: ``apt-get`` upgrade failed"
+if [[ "$IS_SUDO" == true ]]; then
+  if [[ "$OS_PREFIX" == "UBUNTU" ]]; then
+    printf "${REPLACE}${NC}${STAGE}\t\t${YELLOW}%s${NC}\t%s${NC}\n" "CHECKING"
+    if [[ ! -f "$APT_GET" ]]; then
+      printf "${REPLACE}${NC}${STAGE}\t\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "apt-get missing"
       exit 255
-    fi
-  fi
-  if [ -f ~/.apt_sources ]; then
-    MD5_HASH=$(md5sum ~/.apt_sources | cut -d ' ' -f 1)
-    if [[ "$MD5_HASH" != "$MD5_APT_SOURCES" ]]; then
-      echo " :: Installing ``apt-get`` sources"
-      if [ -z "$MD5_APT_SOURCES" ]; then
-        echo "export MD5_APT_SOURCES=$MD5_HASH" >> $ENVIRONMENT
-      else
-        sed -i "s/$MD5_APT_SOURCES/$MD5_HASH/" "$ENVIRONMENT"
+    else
+      eval $SUDO -E -n $APT_GET -qq update
+      if [[ $? != 0 ]]; then
+        printf "${REPLACE}${NC}${STAGE}\t\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "apt-get update failed"
+        exit 255
       fi
-      while read src; do
-        sudo add-apt-repository $src &>/dev/null
-      done < ~/.apt_sources
-    fi
-    unset MD5_HASH
-  fi
-  if [ -f ~/.apt_apps ]; then
-    MD5_HASH=$(md5sum ~/.apt_apps | cut -d ' ' -f 1)
-    if [[ "$MD5_HASH" != "$MD5_APT_APPS" ]]; then
-      echo " :: Installing ``apt-get`` required apps"
-      if [ -z "$MD5_APT_APPS" ]; then
-        echo "export MD5_APT_APPS=$MD5_HASH" >> $ENVIRONMENT
-      else
-        sed -i "s/$MD5_APT_APPS/$MD5_HASH/" "$ENVIRONMENT"
+
+      APT_UPDATE=$(eval $SUDO -E -n $APT_GET -qq upgrade --dry-run)
+      if [[ $? != 0 ]]; then
+        printf "${REPLACE}${NC}${STAGE}\t\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "apt-get upgrade failed"
+        exit 255
       fi
-      while read app; do
-        APP_INSTALLED=$(which $app)
-        if [ -z "$APP_INSTALLED" ]; then
-          sudo apt-get -qq install $app &>/dev/null
+
+      if [[ ! -z "$APT_UPDATE" ]]; then
+        printf "${REPLACE}${NC}${STAGE}\t\t${YELLOW}%s${NC}\t%s${NC}\n" "UPGRADE"
+        APT_UPGRADE=$(eval $SUDO -E -n $APT_GET -qq upgrade -y)
+        if [[ $? != 0 ]]; then
+          printf "${REPLACE}${NC}${STAGE}\t\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "apt-get upgrade failed"
+          exit 255
         fi
-        unset APP_INSTALLED
-      done < ~/.apt_apps
+
+        unset APT_UPDATE
+      fi
+
+      APT_UPDATE=$(eval $SUDO -E -n $APT_GET -qq dist-upgrade --dry-run)
+      if [[ $? != 0 ]]; then
+        printf "${REPLACE}${NC}${STAGE}\t\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "apt-get dist-upgrade failed"
+        exit 255
+      fi
+
+      if [[ ! -z "$APT_UPDATE" ]]; then
+        printf "${REPLACE}${NC}${STAGE}\t\t${YELLOW}%s${NC}\t%s${NC}\n" "DIST-UPGRADE"
+        APT_UPGRADE=$(eval $SUDO -E -n $APT_GET -qq dist-upgrade -y)
+        if [[ $? != 0 ]]; then
+          printf "${REPLACE}${NC}${STAGE}\t\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "apt-get dist-upgrade failed"
+          exit 255
+        fi
+
+        unset APT_UPDATE
+      fi
+
+      if [[ -f "$APT_SOURCES" ]]; then
+        MD5=$(which md5sum)
+        printf "${REPLACE}${NC}${STAGE}\t\t${YELLOW}%s${NC}\t%s${NC}\n" "SOURCES"
+        MD5_HASH=$(eval $MD5 "$APT_SOURCES" | cut -d ' ' -f 1)
+        if [[ "$MD5_HASH" != "$MD5_APT_ADD_SRC" ]]; then
+          while read src; do
+            printf "${REPLACE}${NC}${STAGE}\t\t${YELLOW}%s${NC}\t%s${NC}\n" "SOURCES" "$src"
+            eval $SUDO $APT_ADD_SRC $src &>/dev/null
+            if [[ $? != 0 ]]; then
+              printf "${REPLACE}${NC}${STAGE}\t\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "$APT_ADD_SRC $src failed"
+              exit 255
+            fi
+          done < "$APT_SOURCES"
+
+          if [[ -z "$MD5_APT_ADD_SRC" ]]; then
+            echo "export MD5_APT_ADD_SRC=$MD5_HASH" >> $ENVIRONMENT
+          else
+            sed -i "s/$MD5_APT_ADD_SRC/$MD5_HASH/" "$ENVIRONMENT"
+          fi
+        fi
+
+        unset MD5
+        unset MD5_HASH
+      fi
+
+      if [ -f "$APT_APPS" ]; then
+        MD5=$(which md5sum)
+        printf "${REPLACE}${NC}${STAGE}\t\t${YELLOW}%s${NC}\t%s${NC}\n" "PACKAGES"
+        MD5_HASH=$(eval $MD5 "$APT_APPS" | cut -d ' ' -f 1)
+        if [[ "$MD5_HASH" != "$MD5_APT_APPS" ]]; then
+          while read app; do
+            APP_INSTALLED=$(which $app)
+            if [[ -z "$APP_INSTALLED" ]]; then
+              printf "${REPLACE}${NC}${STAGE}\t\t${YELLOW}%s${NC}\t%s${NC}\n" "PACKAGES" $app
+              eval $SUDO $APT_GET -qq install $app &>/dev/null
+              if [[ $? != 0 ]]; then
+                printf "${REPLACE}${NC}${STAGE}\t\t${RED}%s${NC}\t%s${NC}\n" "ERROR" "apt-get install $app failed"
+                exit 255
+              fi
+            fi
+            unset APP_INSTALLED
+          done < "$APT_APPS"
+
+          if [[ -z "$MD5_APT_APPS" ]]; then
+            echo "export MD5_APT_APPS=$MD5_HASH" >> $ENVIRONMENT
+          else
+            sed -i "s/$MD5_APT_APPS/$MD5_HASH/" "$ENVIRONMENT"
+          fi
+        fi
+
+        unset MD5
+        unset MD5_HASH
+      fi
     fi
-    unset MD5_HASH
+
+    printf "${REPLACE}${NC}${STAGE}\t\t${GREEN}%s${NC}\n" "OK"
   fi
+else
+    printf "${REPLACE}${NC}${STAGE}\t\t${GREEN}%s${NC}\t%s${NC}\n" "SKIPPING" "sudo required"
 fi
 
-echo ":: Verifying user shell"
+STAGE=":: Verifying default shell"
+printf "${NC}%s${NC}\n" "$STAGE"
+printf "${REPLACE}${NC}${STAGE}\t${YELLOW}%s${NC}\t%s${NC}\n" "CHECKING"
 USER_SHELL=$(basename $SHELL)
 if [ "$USER_SHELL" != "zsh" ]; then
   ZSH=$(which zsh)
   if [ -z $ZSH ]; then
-    if [ "$OS_PREFIX" == "osx" ]; then
+    if [ "$OS_PREFIX" == "OSX" ]; then
       eval $BREW install zsh &>/dev/null
       if [ $? != 0 ]; then
         echo ":: ERROR: ``zsh`` install failed"
         exit 255
       fi
-    elif [ "$OS_PREFIX" == "ubuntu" ]; then
-      echo "Install on ubuntu"
+    elif [ "$OS_PREFIX" == "UBUNTU" ]; then
       sudo apt-get -qq install zsh -y
       if [ $? != 0 ]; then
         echo ":: ERROR: ``zsh`` install failed"
@@ -275,11 +367,14 @@ if [ "$USER_SHELL" != "zsh" ]; then
       exit 255
     fi
   fi
+else
+  printf "${REPLACE}${NC}${STAGE}\t${GREEN}%s${NC}\t%s${NC}\n" "OK"
 fi
 
-
-echo ":: Verifying ``.dotfiles`` updates"
-
+unset APT_ADD_SRC
+unset APT_APPS
+unset APT_GET
+unset APT_SOURCES
 unset BREW
 unset BREW_UPDATES
 unset CONFIGURE_DOT_FILES
@@ -290,15 +385,20 @@ unset ENVIRONMENT
 unset GIT
 unset GREEN
 unset INSTALL_DOT_FILES
+unset IS_SUDO
+unset MD5
+unset MD5_APT_ADD_SRC
 unset NC
-unset UPDATE_DOT_FILES
+unset NPM
 unset OS_PREFIX
+unset PIP3
 unset RED
 unset REPLACE
 unset REPLACE2
+unset SUDO
 unset USER_SHELL
-unset ZSH
+unset UPDATE_DOT_FILES
 unset YELLOW
+unset ZSH
 
 rm ~/.update_in_progress
-echo ":: Verified environment"
