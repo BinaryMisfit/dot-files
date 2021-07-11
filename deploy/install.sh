12c70578964fdfb428ebf8d124e3e902cb3416dc
Willie Roberts <diagoza@me.com>
2021-07-11 11:26:28 +0200

    Updated tmux conf

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -e
+OPTIND=1
+REMOTE_REPO=https://github.com/BinaryMisfit/dot-files
 BASE_DIR="${HOME}/.dotfiles"
 CONFIG_SUFFIX=".conf.yaml"
 DEFAULT_CONFIG_PREFIX="default"
@@ -8,6 +9,23 @@ DOT_BOT_DIR="${BASE_DIR}/dotbot"
 DOT_BOT_BIN="bin/dotbot"
 FINAL_CONFIG_PREFIX="final"
 INSTALL_CONFIG_PREFIX="install"
+VERBOSE=0
+UPDATE=1
+
+while getopts "Qv" OPT; do
+  case "${OPT}" in
+    Q)
+      UPDATE=0
+      ;;
+    v)
+      VERBOSE=1
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] &&  shift
 
 OS_PREFIX=
 case "${OSTYPE}" in
@@ -23,39 +41,57 @@ case "${OSTYPE}" in
   ;;
 esac
 
-if [ "${1}" == "-vv" ]; then
+if [ "${VERBOSE}" == "1" ]; then
   echo "Found ${OS_PREFIX}"
 fi
 
 if [ ! -d "${BASE_DIR}" ]; then
-  bash -c "unset HOME; git clone --depth 1 --recurse-submodules --quiet \
-    https://github.com/BinaryMisfit/dot-files \"${BASE_DIR}\""
+  if [ "${VERBOSE}" == "1" ]; then
+    echo "Installing dotfiles"
+  fi
+
+  if [ "${VERBOSE}" == "1" ]; then
+   bash -c "unset HOME; git clone --depth 1 --recurse-submodules \
+    \"${REMOTE_REPO}\" \"${BASE_DIR}\""
+  else
+    bash -c "unset HOME; git clone --depth 1 --recurse-submodules --quiet \
+      \"${REMOTE_REPO}\" \"${BASE_DIR}\""
+  fi
 fi
 
 if [ ! -f "${DOT_BOT_DIR}/${DOT_BOT_BIN}" ]; then
-  if [ "${1}" == "-vv" ]; then
+  if [ "${VERBOSE}" == "1" ]; then
     echo "Updating dotbot"
   fi
 
-  bash -c "unset HOME; git -C \"${BASE_DIR}\" submodule update --init --recursive --quiet"
+  if [ "${VERBOSE}" == "1" ]; then
+    bash -c "unset HOME; git -C \"${BASE_DIR}\" submodule update --init --recursive"
+  else
+    bash -c "unset HOME; git -C \"${BASE_DIR}\" submodule update --init --recursive --quiet"
+  fi
 fi
 
-if [ "${1}" == "-vv" ]; then
-  echo "Updating repository"
-fi
+if [ "${UPDATE}" == "1" ]; then
+  if [ "${VERBOSE}" == "1" ]; then
+    echo "Updating repository"
+  fi
 
-bash -c "unset HOME; git -C \"${BASE_DIR}\" pull --autostash --all --recurse-submodules --quiet"
+  if [ "${VERBOSE}" == "1" ]; then
+    bash -c "unset HOME; git -C \"${BASE_DIR}\" pull --autostash --all --recurse-submodules"
+  else
+    bash -c "unset HOME; git -C \"${BASE_DIR}\" pull --autostash --all --recurse-submodules --quiet"
+  fi
+fi
 
 for CONF in ${DEFAULT_CONFIG_PREFIX} ${INSTALL_CONFIG_PREFIX} ${OS_PREFIX} ${FINAL_CONFIG_PREFIX} "${@}"; do
   if [ "${CONF}" == "-vv" ]; then
     continue
   fi
 
-  if [ "${1}" == "-vv" ]; then
+  if [ "${VERBOSE}" == "1" ]; then
     echo "Applying ${CONF}"
     "${DOT_BOT_DIR}/${DOT_BOT_BIN}" -vv -d "${BASE_DIR}" -c "${DEPLOY_DIR}/${CONF}${CONFIG_SUFFIX}"
   else
-    "${DOT_BOT_DIR}/${DOT_BOT_BIN}" -d "${BASE_DIR}" -c "${DEPLOY_DIR}/${CONF}${CONFIG_SUFFIX}"
+    "${DOT_BOT_DIR}/${DOT_BOT_BIN}" -q -d "${BASE_DIR}" -c "${DEPLOY_DIR}/${CONF}${CONFIG_SUFFIX}"
   fi
-
 done
