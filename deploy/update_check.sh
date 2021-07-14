efde217f447302e2af62bea68ea60a0dcae10777
Willie Roberts <diagoza@me.com>
2021-07-14 13:40:33 +0200

    Only pull update if new

--- deploy/update_check.sh
+++ deploy/update_check.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 BASE_DIR="${HOME}/.dotfiles"
 if [[ -d "${BASE_DIR}" ]]; then
-  pushd "${BASE_DIR}" > /dev/null
-  VERSION_CURRENT=$(git rev-parse HEAD)
-  bash -c "unset HOME; git pull --autostash --all --recurse-submodules --rebase --quiet 2>&1 > /dev/null"
-  VERSION_NEW=$(git rev-parse HEAD)
+  BRANCH=$(git -C "${BASE_DIR}" name-rev --name-only HEAD)
+  VERSION_CURRENT=$(git -C "${BASE_DIR}" rev-parse HEAD)
+  VERSION_NEW=$(git -C "${BASE_DIR}" rev-parse origin/${BRANCH})
   if [[ "${VERSION_CURRENT}" != "${VERSION_NEW}" ]]; then
+    bash -c "unset HOME; git -C "${BASE_DIR}" pull --autostash --all --recurse-submodules --rebase --quiet 2>&1 > /dev/null"
     printf "\033[0;31mConfig installed ${VERSION_CURRENT}, online: ${VERSION_NEW}, updating\033[0m\n"
     . "${BASE_DIR}/install" -Q
   else
     printf "\033[0;32mConfig installed: ${VERSION_CURRENT}, online: ${VERSION_NEW}, up-to-date\033[0m\n"
   fi
-  popd > /dev/null
+  unset BRANCH
   unset VERSION_CURRENT
   unset VERSION_NEW
 fi
