b1458d277362e1fc7dcb1fdf3b78588f0346f0e8
Willie Roberts <diagoza@me.com>
2021-07-14 12:35:17 +0200

    Additional fixes to remote update #5

--- deploy/update_check.sh
+++ deploy/update_check.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 BASE_DIR="${HOME}/.dotfiles"
 if [[ -d "${BASE_DIR}" ]]; then
-  VERSION_CURRENT=$(git -C "${BASE_DIR}" rev-parse HEAD)
-  bash -c "unset HOME; git -C \"${BASE_DIR}\" pull --autostash --all --recurse-submodules --rebase --quiet 2>&1 > /dev/null"
-  VERSION_NEW=$(git -C "${BASE_DIR}" rev-parse HEAD)
+  pushd "${BASE_DIR}" > /dev/null
+  VERSION_CURRENT=$(git rev-parse HEAD)
+  bash -c "unset HOME; git pull --autostash --all --recurse-submodules --rebase --quiet 2>&1 > /dev/null"
+  VERSION_NEW=$(git rev-parse HEAD)
   if [[ "${VERSION_CURRENT}" != "${VERSION_NEW}" ]]; then
     printf "\033[0;31mConfig installed ${VERSION_CURRENT}, online: ${VERSION_NEW}, updating\033[0m\n"
-    pushd "${BASE_DIR}" > /dev/null
     install -Q
-    popd > /dev/null
   else
     printf "\033[0;32mConfig installed: ${VERSION_CURRENT}, online: ${VERSION_NEW}, up-to-date\033[0m\n"
   fi
+  popd > /dev/null
   unset VERSION_CURRENT
   unset VERSION_NEW
 fi
