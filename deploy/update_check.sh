5d4c690d36ff2fe6c98723e06c3cc408b61be839
Willie Roberts <diagoza@me.com>
2021-07-14 12:16:34 +0200

    Fixed online update check

--- deploy/update_check.sh
+++ deploy/update_check.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-BASE_DIR="${USER}/.dotfiles"
+BASE_DIR="${HOME}/.dotfiles"
 if [[ -d "${BASE_DIR}" ]]; then
   VERSION_CURRENT=$(git -C "${BASE_DIR}" rev-parse HEAD)
-  bash -c "unset HOME; git -C \"${BASE_DIR}\" pull --autostash --all --recurse-submodules --rebase --quiet 2>&1 /dev/null"
+  bash -c "unset HOME; git -C \"${BASE_DIR}\" pull --autostash --all --recurse-submodules --rebase --quiet 2>&1 > /dev/null"
   VERSION_NEW=$(git -C "${BASE_DIR}" rev-parse HEAD)
   if [[ "${VERSION_CURRENT}" != "${VERSION_NEW}" ]]; then
-    exec "${BASE_DIR}/install" -Q
+    printf "\033[0;31mConfig installed ${VERSION_CURRENT}, online: ${VERSION_NEW}, updating\033[0m\n"
+    source "${BASE_DIR}/install" -Q
+  else
+    printf "\033[0;32mConfig installed: ${VERSION_CURRENT}, online: ${VERSION_NEW}, up-to-date\033[0m\n"
   fi
   unset VERSION_CURRENT
   unset VERSION_NEW
