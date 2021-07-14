637d2e7ba042c931f6c7bbf3c113a7f6e179da95
Willie Roberts <diagoza@me.com>
2021-07-14 12:22:12 +0200

    Additional fixes to remote update

--- deploy/update_check.sh
+++ deploy/update_check.sh
@@ -6,7 +6,9 @@ if [[ -d "${BASE_DIR}" ]]; then
   VERSION_NEW=$(git -C "${BASE_DIR}" rev-parse HEAD)
   if [[ "${VERSION_CURRENT}" != "${VERSION_NEW}" ]]; then
     printf "\033[0;31mConfig installed ${VERSION_CURRENT}, online: ${VERSION_NEW}, updating\033[0m\n"
-    source "${BASE_DIR}/install" -Q
+    pushd -q "${BASE_DIR}"
+    install -Q
+    popd -q
   else
     printf "\033[0;32mConfig installed: ${VERSION_CURRENT}, online: ${VERSION_NEW}, up-to-date\033[0m\n"
   fi
