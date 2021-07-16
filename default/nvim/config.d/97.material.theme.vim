7d552411cde57909189795f4559904a614923bd1
Willie Roberts <diagoza@me.com>
2021-07-16 13:15:43 +0200

    Updated neovim to material theme

---
+++ default/nvim/config.d/97.material.theme.vim
@@ -0,0 +1,3 @@
+if !exists('g:vscode')
+  silent! colorscheme material
+endif
--- platform/ubuntu/scripts/tools/ssl_install.sh
+++ platform/ubuntu/scripts/tools/ssl_install.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+LE_DOMAIN=${Le_Domain}
+LE_WORK_DIR=${LE_WORKING_DIR}
+DOMAIN_DIR="${LE_WORK_DIR}/${LE_DOMAIN}"
+SERVICE_NAME=$(echo ${LE_DOMAIN} | cut -d"." -f1)
+printf "$LE_DOMAIN"
+printf "$LE_WORK_DIR"
+printf "$DOMAIN_DIR"
+printf "$SERVICE_NAME"
