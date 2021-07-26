7b999e86b1755aa66900908344022d91ee4d9f09
Willie Roberts <diagoza@me.com>
2021-07-26 13:56:34 +0200

    Fixed color codes for update check

--- deploy/update_check.sh
+++ deploy/update_check.sh
@@ -10,7 +10,7 @@ if [[ -d "${BASE_DIR}" ]]; then
   VERSION_NEW=$(git ls-remote https://github.com/BinaryMisfit/dot-files HEAD | awk '{ print $1 }')
   printf "\033[3;93mFound\t\033[3;97m${VERSION_CURRENT}\033[0m\n"
   if [[ "${VERSION_CURRENT}" != "${VERSION_NEW}" ]]; then
-    printf "\033[3;93mLatest\t\033[3;97m${VERSION_NEW}\t\033[3;91m[Updating]\033[0m\n"
+    printf "\033[3;93mLatest\t\033[3;97m${VERSION_NEW}, \033[3;91mupdating\033[0m\n"
     bash -c "unset HOME; git -C "${BASE_DIR}" pull --autostash --all --recurse-submodules --rebase --quiet 2>&1 > /dev/null"
     pushd ${BASE_DIR} > /dev/null
     . "${BASE_DIR}/install" -Qs
