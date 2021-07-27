7b87a903a9b02c9268adc11eedf410c4e38b7f29
Willie Roberts <diagoza@me.com>
2021-07-27 16:42:15 +0200

    Updated online check scripts

--- deploy/update_check.sh
+++ deploy/update_check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-printf "\033[0;92m[  ..  ]\033[0m Online update\033[0m"
 if [[ ! -z ${TMUX} ]]; then
+  printf "\r\033[0;93m[ SKIP ]\033[0m Online update\033[0m\n"
   exit 0
 fi
 
@@ -9,16 +9,16 @@ if [[ -d "${BASE_DIR}" ]]; then
   BRANCH=$(git -C "${BASE_DIR}" name-rev --name-only HEAD)
   VERSION_CURRENT=$(git -C "${BASE_DIR}" rev-parse HEAD)
   VERSION_NEW=$(git ls-remote https://github.com/BinaryMisfit/dot-files HEAD | awk '{ print $1 }')
-  printf "\033[3;93mFound\t\033[3;97m${VERSION_CURRENT}\033[0m\n"
   if [[ "${VERSION_CURRENT}" != "${VERSION_NEW}" ]]; then
-    printf "\033[3;93mLatest\t\033[3;91m${VERSION_NEW}, updating\033[0m\n"
+    printf "\r\033[0;93m[UPDATE]\033[0m Online update\033[0m\n"
     bash -c "unset HOME; git -C "${BASE_DIR}" pull --autostash --all --recurse-submodules --rebase --quiet 2>&1 > /dev/null"
     pushd ${BASE_DIR} > /dev/null
     . "${BASE_DIR}/install" -Qs
     popd > /dev/null
   else
-    printf "\033[3;93mLatest\t\033[3;97m${VERSION_NEW}\033[0m\n"
+    printf "\r\033[0;92m[  OK  ]\033[0m Online update\033[0m\n"
   fi
+
   unset BRANCH
   unset VERSION_CURRENT
   unset VERSION_NEW
--- deploy/update_online.sh
+++ deploy/update_online.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+printf "\033[0;92m[  ..  ]\033[0m Online update\033[0m"
 if [[ ! -z ${TMUX} ]]; then
+  printf "\r\033[0;93m[ SKIP ]\033[0m Online update\033[0m\n"
   exit 0
 fi
 
-printf "\033[3;93m==> Checking online for update\033[0m\n"
 if echo -e "GET http://raw.github.com HTTP/1.0\n\n" | nc github.com 80 >/dev/null 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.github.com/BinaryMisfit/dot-files/active/deploy/update_check.sh)"
 fi
