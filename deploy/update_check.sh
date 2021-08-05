c2038469641da587b65a1b145972ee2534d7b5d5
Willie Roberts <wroberts@mrpg.com>
2021-08-05 11:13:27 +0200

    Debugging online update script

--- deploy/update_check.sh
+++ deploy/update_check.sh
@@ -11,6 +11,8 @@ if [[ -d "${BASE_DIR}" ]]; then
   if [[ "${VERSION_CURRENT}" != "${VERSION_NEW}" ]]; then
     printf "\r\033[0;93m[UPDATE]\033[0;97m Online update\033[0m"
     COMMAND="unset HOME; git -C \"${BASE_DIR}\" pull --autostash --all --recurse-submodules --rebase --quiet"
+    OUTPUT=$(bash -c "${COMMAND}")
+    printf "\n%s" "${OUTPUT}"
     if bash -c "${COMMAND}" >/dev/null; then
       COMMAND="\"${BASE_DIR}\"/install -s"
       if ! bash -c "${COMMAND}"; then
