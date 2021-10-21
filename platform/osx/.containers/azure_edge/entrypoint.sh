82b02dc1a1e1666971ed24590bf54e69c95c19fa
Willie Roberts <diagoza@me.com>
2021-10-21 16:42:42 +0200

    Updated entrypoint

--- platform/osx/.containers/azure_edge/entrypoint.sh
+++ platform/osx/.containers/azure_edge/entrypoint.sh
@@ -1,7 +1,11 @@
 ﻿#!/usr/bin/env bash
 
-if [ -d /sqlmaster/ ]; then
+if [[ -d /sqlmaster/ ]]; then
   chown -R mssql:mssql /sqlmaster
+fi
+
+if [[ -d /sqldata/ ]]; then
+  chown -R mssql:mssql /sqldata
 fi
 
 /opt/mssql/bin/sqlservr
