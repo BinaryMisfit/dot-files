55f0b2cdd2ab33301a330bbeafcff318d8863401
Willie Roberts <diagoza@me.com>
2021-11-03 13:20:42 +0200

    Added shared script to default

--- deploy/update_online.sh
+++ deploy/update_online.sh
@@ -5,6 +5,11 @@ if [[ -n ${TMUX+x} ]]; then
   exit 0
 fi
 
+if [[ -n ${TERMINAL_EMULATOR+x} ]]; then
+  printf "\r\033[0;96m[ SKIP ]\033[0;96m Online update\033[0m\n"
+  exit 0
+fi
+
 if echo -e "GET http://raw.github.com HTTP/1.0\n\n" | nc github.com 80 >/dev/null 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.github.com/BinaryMisfit/dot-files/active/deploy/update_check.sh)"
 fi
