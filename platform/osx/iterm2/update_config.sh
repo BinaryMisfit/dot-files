5c3d47b095ad2d35c978ff46b7598039b7811d90
Willie Roberts <diagoza@me.com>
2021-11-06 10:59:26 +0200

    Updated iterm2

--- platform/osx/iterm2/update_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ -d "/Applications/iTerm2.app" ]; then
-  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2"
-  defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
-fi
